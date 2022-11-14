f012e6e06dc8807e655b5473e5cc51669cc4e4a1
Claudio <claudio.vozza@buonarroti.tn.it>
2022-11-14 21:15:54 +0100

    Update build files (again)

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cd ./Modules/SDL2
 mkdir -p build && cd build
 export PREFIX=$DIR/3rdparty/SDL2
 echo $DIR
-cmake -A x64 .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build . --config Release
 cmake --config Release --install=$PREFIX .
 cp ./include/* $DIR/3rdparty/SDL2/
@@ -15,7 +15,7 @@ cp ./include-config-release/* $DIR/3rdparty/SDL2/
 cp ./Release/* $DIR/3rdparty/libs/
 cd ../../Poco
 mkdir -p build_poco && cd build_poco
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -A x64 ..
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
 cmake --build . --target install --parallel --config Release
 cp ./bin/release/*.dll $DIR/3rdparty/libs/
 cp ./lib/release/*.lib $DIR/3rdparty/libs/
