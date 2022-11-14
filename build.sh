110250f9af2935a3d88370d1aaba8795ac4ae847
Claudio <claudio.vozza@buonarroti.tn.it>
2022-11-14 21:36:55 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -7,24 +7,12 @@ cd ./Modules/SDL2
 mkdir -p build && cd build
 export PREFIX=$DIR/3rdparty/SDL2
 echo $DIR
-cmake .. -DCMAKE_BUILD_TYPE=Release
-cmake --build . --config Release
-cmake --config Release --install=$PREFIX .
-cp ./include/* $DIR/3rdparty/SDL2/
-cp ./include-config-release/* $DIR/3rdparty/SDL2/
-cp ./Release/* $DIR/3rdparty/libs/
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX .. -DCMAKE_BUILD_TYPE=Release
+cmake --build . --target install --config Release
+export PREFIX=$DIR/3rdparty/Poco
 cd ../../Poco
 mkdir -p build_poco && cd build_poco
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
 cmake --build . --target install --parallel --config Release
-cp ./bin/release/*.dll $DIR/3rdparty/libs/
-cp ./lib/release/*.lib $DIR/3rdparty/libs/
-cd ..
-cp -r ./Foundation/include/Poco/* $DIR/3rdparty/Poco/
-cp -r ./JSON/include/Poco/* $DIR/3rdparty/Poco/
-cp -r ./Net/include/Poco/* $DIR/3rdparty/Poco/
-cp -r ./Util/include/Poco/* $DIR/3rdparty/Poco/
-cp -r ./XML/include/Poco/* $DIR/3rdparty/Poco/
-cp -r ./Data/include/Poco/* $DIR/3rdparty/Poco/
 cd $DIR
 cmake .
