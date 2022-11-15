86714291a074f196b1d201d1a41001e2f843148d
Claudio <claudio.vozza@buonarroti.tn.it>
2022-11-15 19:08:27 +0100

    Update build scripts

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ mkdir 3rdparty
 mkdir 3rdparty/libs
 mkdir 3rdparty/SDL2
 mkdir 3rdparty/Poco
+mkdir 3rdparty/fmt
 cd ./Modules/SDL2
 mkdir -p build && cd build
 export PREFIX=$DIR/3rdparty/SDL2
@@ -14,5 +15,10 @@ cd ../../Poco
 mkdir -p build_poco && cd build_poco
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
 cmake --build . --target install --parallel --config Release
+cd ../../fmt
+export PREFIX=$DIR/3rdparty/fmt
+mdkir -p build && cd build
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
+cmake --build . --target install --config Release
 cd $DIR
 cmake .
